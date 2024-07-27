7787c229023019eb2edb302426f9067b19a58a02
oceanpan <ocean@192.168.1.3>
2024-07-27 21:27:03 +0800

    improve dao

--- src/main/java/com/demo/jdbc_template/transaction/AccountService.java
+++ src/main/java/com/demo/jdbc_template/transaction/AccountService.java
@@ -2,6 +2,7 @@ package com.demo.jdbc_template.transaction;
 
 import com.demo.jdbc_template.CodeConfig_DataSource.AccountRowMapper;
 import com.demo.jdbc_template.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
@@ -14,6 +15,8 @@ import java.util.List;
 @Service
 public class AccountService {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     /**
      * 先插入两条测试数据
@@ -23,11 +26,11 @@ public class AccountService {
         ClassPathXmlApplicationContext classPathXmlApplicationContext =
                 new ClassPathXmlApplicationContext("jdbc_template/transaction.xml");
         JdbcTemplate jdbcTemplate = classPathXmlApplicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
-        int id = 1;
+        int id = 5;
         String name = "xm2";
         double balance = 1000;
 
-        int id2 = 2;
+        int id2 = 6;
         String name2 = "xh2";
         double balance2 = 1000;
 
@@ -39,27 +42,19 @@ public class AccountService {
     }
 
 
-    /**
-     * 测试发现事务不生效
-     */
-    @Test
     @Transactional
     public void transfer(){
-        ClassPathXmlApplicationContext classPathXmlApplicationContext =
-                new ClassPathXmlApplicationContext("jdbc_template/transaction.xml");
-        JdbcTemplate jdbcTemplate = classPathXmlApplicationContext.getBean("jdbcTemplate", JdbcTemplate.class);
-
         String querySql = "select * from account where id=?";
-        List<Account> accounts1 = jdbcTemplate.query(querySql, new AccountRowMapper(), 1);
-        List<Account> accounts2 = jdbcTemplate.query(querySql, new AccountRowMapper(), 2);
+        List<Account> accounts1 = jdbcTemplate.query(querySql, new AccountRowMapper(), 3);
+        List<Account> accounts2 = jdbcTemplate.query(querySql, new AccountRowMapper(), 4);
         double balance1 = accounts1.get(0).getBalance();
         double balance2 = accounts2.get(0).getBalance();
         System.out.println("balance1 = " + balance1);
         System.out.println("balance2 = " + balance2);
 
 
-        String updateSql = "update account set balance=" + (balance1 - 10) + "where id=1";
-        String updateSql2 = "update account set balance=" + (balance2 + 10) + "where id=2";
+        String updateSql = "update account set balance=" + (balance1 - 10) + "where id=3";
+        String updateSql2 = "update account set balance=" + (balance2 + 10) + "where id=4";
 
         jdbcTemplate.update(updateSql);
         //模拟突然出现的异常，比如银行中可能为突然停电等等
@@ -68,5 +63,11 @@ public class AccountService {
         jdbcTemplate.update(updateSql2);
     }
 
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext classPathXmlApplicationContext =
+                new ClassPathXmlApplicationContext("jdbc_template/transaction.xml");
+        AccountService accountService = classPathXmlApplicationContext.getBean("accountService", AccountService.class);
+        accountService.transfer();
+    }
 
 }
